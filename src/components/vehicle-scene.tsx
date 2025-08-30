cc7f7168ad42aa619802a933bc8fc885e6db918b
Tuomas Hatakka <tuomas.hatakka@gmail.com>
2025-08-30 13:47:12 +0000

    Replace the vehicle model with /public/spaceship_-_cb1/scene.gltf

--- src/components/vehicle-scene.tsx
+++ src/components/vehicle-scene.tsx
@@ -3,7 +3,7 @@
 
 import { useBox, useRaycastVehicle, useSphere } from '@react-three/cannon';
 import { useFrame, useLoader } from '@react-three/fiber';
-import { useEffect, useRef, forwardRef, useCallback } from 'react';
+import { useEffect, useRef } from 'react';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { useControls } from '@/hooks/use-mobile';
 import { useStore } from '@/hooks/use-store';
@@ -11,34 +11,11 @@ import { vehicleConfig, wheelInfos } from '@/lib/utils';
 import type { Object3D } from 'three';
 import { Quaternion, Vector3, Group } from 'three';
 
-const Wheel = forwardRef<Group, { radius: number }>(({ radius }, ref) => {
-  const wheelGltf = useLoader(GLTFLoader, 'https://tuomashatakka.github.io/public/resources/models/vehicles/vorsteiner_v-ff109/scene.gltf');
-  
-  useEffect(() => {
-    if (wheelGltf.scene) {
-      wheelGltf.scene.traverse((child) => {
-        if ('isMesh' in child && child.isMesh) {
-          child.castShadow = true;
-          child.receiveShadow = true;
-        }
-      });
-    }
-  }, [wheelGltf]);
-
-  return (
-    <group ref={ref}>
-      <primitive object={wheelGltf.scene.clone()} rotation={[0, -Math.PI / 2, 0]} scale={0.026} />
-    </group>
-  );
-});
-
-Wheel.displayName = 'Wheel';
-
 export function Vehicle() {
   const { controls } = useControls();
   const setSpeed = useStore((state) => state.setSpeed);
   
-  const carGltf = useLoader(GLTFLoader, 'https://tuomashatakka.github.io/public/resources/models/vehicles/honda_s2000_gt_ap2/scene.gltf');
+  const carGltf = useLoader(GLTFLoader, '/public/spaceship_-_cb1/scene.gltf');
   
   const position: [number, number, number] = [0, 2, 0];
   const { radius } = vehicleConfig;
@@ -56,7 +33,6 @@ export function Vehicle() {
     chassisRef
   );
 
-  const wheelRefs = [useRef<Object3D>(null), useRef<Object3D>(null), useRef<Object3D>(null), useRef<Object3D>(null)];
   const wheelBodies = [useRef<Object3D>(null), useRef<Object3D>(null), useRef<Object3D>(null), useRef<Object3D>(null)];
   
   useSphere(() => ({ mass: 1, type: 'Kinematic', collisionFilterGroup: 0, args: [radius] }), wheelBodies[0]);
@@ -150,12 +126,8 @@ export function Vehicle() {
   return (
     <group ref={vehicle as React.Ref<Group>}>
       <group ref={chassisRef}>
-        <primitive object={carGltf.scene} position={[0, -0.5, 0]}/>
+        <primitive object={carGltf.scene} position={[0, -0.8, 0]}/>
       </group>
-      <Wheel ref={wheelRefs[0]} radius={radius} />
-      <Wheel ref={wheelRefs[1]} radius={radius} />
-      <Wheel ref={wheelRefs[2]} radius={radius} />
-      <Wheel ref={wheelRefs[3]} radius={radius} />
     </group>
   );
 }
