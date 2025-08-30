60134130942f9d0e872f568a98eba89548f0a60f
Tuomas Hatakka <tuomas.hatakka@gmail.com>
2025-08-30 16:31:46 +0000

    Disable the obstacles for now and remove the obstacles components

--- src/components/levels/procedural.tsx
+++ src/components/levels/procedural.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { usePlane } from '@react-three/cannon';
+import { usePlane, useBox } from '@react-three/cannon';
 import { MeshReflectorMaterial } from '@react-three/drei';
-import { Box, RotatingBox, SwingingHammer } from '@/components/obstacles';
+import type { BoxProps, Triplet } from '@react-three/cannon';
 
 function Ground() {
   const [ref] = usePlane(() => ({
@@ -29,6 +29,29 @@ function Ground() {
   );
 }
 
+type PhysicalBoxProps = BoxProps & {
+  args?: Triplet;
+  material?: any;
+};
+
+function Box({ args = [2, 2, 2], material = { friction: 0.1 }, ...props }: PhysicalBoxProps) {
+  const [ref] = useBox(() => ({
+    mass: 0, // Make track segments static
+    type: 'Static',
+    args,
+    material,
+    ...props,
+  }));
+
+  return (
+    <mesh ref={ref} castShadow receiveShadow>
+      <boxGeometry args={args} />
+      <meshStandardMaterial color="hsl(var(--primary))" />
+    </mesh>
+  );
+}
+
+
 export default function ProceduralTrack() {
   const trackLength = 20;
   const trackWidth = 8;
@@ -46,27 +69,8 @@ export default function ProceduralTrack() {
         const yOffset = Math.random() * 2 - 1;
 
         // Path segment
-        const segment = <Box key={`segment-${i}`} position={[xOffset, -0.5 + yOffset, z]} args={[trackWidth, 1, 20]} />;
+        return <Box key={`segment-${i}`} position={[xOffset, -0.5 + yOffset, z]} args={[trackWidth, 1, 20]} />;
 
-        // Obstacles for the segment
-        let obstacles = [];
-        const obstacleType = Math.random();
-
-        if (obstacleType < 0.3) {
-          // Swinging Hammers
-          obstacles.push(<SwingingHammer key={`hammer1-${i}`} position={[xOffset - trackWidth / 2 - 2, 5 + yOffset, z]} />);
-          obstacles.push(<SwingingHammer key={`hammer2-${i}`} position={[xOffset + trackWidth / 2 + 2, 5 + yOffset, z]} />);
-        } else if (obstacleType < 0.6) {
-          // Rotating Boxes
-          obstacles.push(<RotatingBox key={`rotbox1-${i}`} position={[xOffset, 1.5 + yOffset, z - 5]} args={[trackWidth - 1, 1, 3]} />);
-           obstacles.push(<RotatingBox key={`rotbox2-${i}`} position={[xOffset, 1.5 + yOffset, z + 5]} args={[trackWidth - 1, 1, 3]} />);
-        } else {
-          // Static Boxes as hurdles
-           obstacles.push(<Box key={`staticbox1-${i}`} position={[xOffset - 2.5, 0.5 + yOffset, z]} args={[2, 1, 2]} material={{friction: 0.1}}/>);
-           obstacles.push(<Box key={`staticbox2-${i}`} position={[xOffset + 2.5, 0.5 + yOffset, z]} args={[2, 1, 2]} material={{friction: 0.1}}/>);
-        }
-
-        return [segment, ...obstacles];
       })}
     </>
   );
